933508e69ff9fb313125034d3c02b6b03fe6ba81
Daniel Troyano <dtroyano86@gmail.com>
2023-05-19 16:37:02 -0500

    update

--- components/About.jsx
+++ components/About.jsx
@@ -10,22 +10,11 @@ const About = () => (
         <p>Hi! I'm Daniel, a software engineer based out of New Orleans.</p>
         <p>
           I've always been very passionate about making things, whether that's woodworking, 
-          making Unity games in C#, or Wordpress templates in PHP. I'm the friend people come 
+          making Unity games in C#, or prototype Board Games. I'm the friend people come 
           to when they need something fixed or made. I've tackled electrician work, built retroPies, 
-          and made websites. No matter the problem I've always enjoyed digging my teeth into 
+          and made furniture. No matter the problem I've always enjoyed digging my teeth into 
           it and get real satisfaction from watching the pieces click into place.
         </p>
-        <p>These are a few of the skills I've been working on lately:</p>
-        <ul className="skills">
-          <li>JavaScript</li>
-          <li>Rails</li>
-          <li>React.js</li>
-          <li>Vue.js</li>
-          <li>Node.js</li>
-          <li>Next.js</li>
-          <li>Sequelize</li>
-          <li>Phaser 3</li>
-        </ul>
       </Col>
       <Col md="3">
         <Image src="/profile.jpg" thumbnail className="profile-image" />
